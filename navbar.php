8dca799d716d0c3456e9a6ced9856f373e9e898f
ashrafmisran <ashrafmisran@gmail.com>
2018-03-26 15:42:49 +0800

    Rename the site

--- navbar.php
+++ navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="index.php">CRSys</a>
+  <a class="navbar-brand" href="index.php">CGD</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -15,16 +15,6 @@
       <li class="nav-item">
         <a class="nav-link" href="?p=order-48-report">Order 48 Daily Report</span></a>
       </li>
-      <!--li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Upload
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="?p=upload-deposit">Deposit record</a>
-          <a class="dropdown-item" href="#">Withdrawal record</a>
-          <a class="dropdown-item" href="#">Order record</a>
-        </div>
-      </li-->
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Shariah
